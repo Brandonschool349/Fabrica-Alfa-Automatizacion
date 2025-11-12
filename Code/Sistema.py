8058f0679f925d29e82d57ab4f6be416ebd47639
Brandonschool349 <bpedrazavaldez@gmail.com>
2025-11-12 08:02:42 -0600

    Test de sincronización

--- Code/Sistema.py
+++ Code/Sistema.py
@@ -1,3 +1,18 @@
-print("hello world")
-print("second test")
-print("third test")
+# sistema.py - Prototipo de sistema estadístico y demo para Fábrica Alfa con la finalidad de ser mostrado a empresarios.
+# Incluira:
+# - Interfaz de usuario (CLI)
+# - Carga de datos (Excel/CSV)
+# - Estadísticas descriptivas y medidas de tendencia central y dispersión
+# - Distribuciones: Binomial, Poisson, Normal
+# - Intervalos de confianza
+# - Pruebas de hipótesis (tests)
+# - ANOVA
+# - Correlación y regresión lineal
+# - Visualizaciones básicas (matplotlib / seaborn)
+# - Exportación de resultados a CSV y PNG
+#
+# Versión: 0.1
+# Fecha: 2024-06-15
+# Autores: Equipo Fábrica Alfa (prototipo académico) Brandon Pedraza, Emilio Zuñiga, Juan Pablo, David Alejandro, Daniel
+
+print("prueba")
