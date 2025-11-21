cec2d19732f35766c5ab799e52638085cd589111
Brandonschool349 <bpedrazavaldez@gmail.com>
2025-11-20 19:50:22 -0600

    Forzar subida de Sistema.py renombrado

---
+++ Code/Sistema_forzar.py
@@ -0,0 +1,23 @@
+# sistema.py - Prototipo de sistema estadístico y demo para Fábrica Alfa con la finalidad de ser mostrado a empresarios.
+# Incluira:
+# - Interfaz de usuario (CLI)
+# - Carga de datos (Excel/CSV)
+# - Estadísticas descriptivas y medidas de tendencia central y dispersión
+# - Distribuciones: Binomial, Poisson, Normal
+# - Intervalos de confianza
+# - Pruebas de hipótesis (tests)
+# - ANOVA
+# - Correlación y regresión lineal
+# - Visualizaciones básicas (matplotlib / seaborn)
+# - Exportación de resultados a CSV y PNG
+#
+# Versión: 0.1
+# Fecha: 2024-06-15
+# Autores: Equipo Fábrica Alfa (prototipo académico) Brandon Pedraza, Emilio Zuñiga, Juan Pablo, David Alejandro, Daniel
+
+print("prueba")
+print("Comit JP Tonpro")
+print("Prueba JP 3")
+print("Brandon C")
+print("Prueba JP 4")
+print("Prueba5")
